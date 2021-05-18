de63705b015a0cf49cd0ed036546fafa8ada28f3
heythor <heitor.fabricio@hotmail.com>
2021-05-18 16:21:12 -0300

    18/05/21

--- ListaWhile2/Questao_10.rb
+++ ListaWhile2/Questao_10.rb
@@ -1,8 +1,14 @@
 numero = 1
-while numero != 150
-
-    if test
-        
+contador = 0
+while numero <=150
+    contador = 0
+    for i in 1..numero
+        if numero % i == 0
+            contador +=1
+        end
     end
-    
+    if contador <= 2
+        puts numero
+    end
+numero +=1    
 end
