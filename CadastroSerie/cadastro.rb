a5735988363c920baca17c108627ac6ca1b13fa4
heythor <heitor.fabricio@hotmail.com>
2021-06-07 14:03:09 -0300

    Adicionando a função editar

--- CadastroSerie/cadastro.rb
+++ CadastroSerie/cadastro.rb
@@ -2,9 +2,10 @@ def menu
     
     "Digite a opção desejada
      
-    1- Adicionar Serie
-    2- Listar Series
-    3- remover Serie
+    1- Adicionar Série
+    2- Listar Séries
+    3- Remover Série
+    4- Editar Série
 
     0- Sair
 "
@@ -19,23 +20,25 @@ quantidade_array = 0
 while opcao > 0
     verificar_serie_existente = 0
     contador_Deletar_Serie = 0
+    contador_Editar_Serie =0
 
     case opcao > 0
 
     when opcao == 1
 
-        puts"Digite o nome da  nova Serie:"
-        serie[quantidade_array] = gets
+        puts"Digite o nome da nova Serie:"
+        nome_serie = gets
         for i in 0.. quantidade_array do
-            if serie[i] == serie[quantidade_array]
+            if serie[i] == nome_serie
                 verificar_serie_existente +=1
             end
         end
 
-        if verificar_serie_existente > 1
+        if verificar_serie_existente > 0
             puts"Serie ja cadastrada"
             quantidade_array -=1
         else
+            serie[quantidade_array] = nome_serie
             quantidade_array +=1
             puts"Serie Cadastrada com suscesso"
         end
@@ -56,6 +59,27 @@ while opcao > 0
         if contador_Deletar_Serie == 0
             puts"Serie digitada nao existe"
         end
+    when opcao == 4
+        
+        puts"Digite o nome da serie que deseja editar:"
+        editar_serie = gets
+
+        for i in 0..quantidade_array do
+            
+            if serie[i] == editar_serie
+                
+                puts"Digite sua edição: "
+                serie[i] = gets
+                puts
+                puts"Alteração Realizada"
+                contador_Editar_Serie +=1
+            end
+        end
+
+        if contador_Editar_Serie == 0
+            puts"Serie digitada nao existe"
+        end
+
     else 
         puts"Opcao invalida"
     end
